e6c2e971a6957073fc3bbf78f8560beed24c8810
Kuzon Chen <kuzoncby@gmail.com>
2016-03-17 23:23:54 +0800

    Generate Model

--- orm/src/main/java/com/kuzonchen/generator/orm.java
+++ orm/src/main/java/com/kuzonchen/generator/orm.java
@@ -9,11 +9,15 @@ public class orm {
 	public static void main(String args[]) throws Exception {
 		//Create Schema
 		Schema schema = new Schema(1, "com.kuzonchen.model");
+		//Add Entity
 		Entity timetable = schema.addEntity("Timetable");
 		timetable.addIdProperty();
 		timetable.addStringProperty("StopName");
 		timetable.addStringProperty("StopTime");
-		new DaoGenerator().generateAll(schema, "../app/src/main/java/com/kuzonchen/model");
+		//Mac or Linux
+//		new DaoGenerator().generateAll(schema, "../app/src/main/java");
+		//Windows
+		new DaoGenerator().generateAll(schema, "app\\src\\main\\java");
 	}
 
 }
